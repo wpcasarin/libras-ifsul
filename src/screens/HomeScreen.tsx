c512e5201a911d9e0b85208f09ff20ff10cd1b59
wpcasarin <72427968+wpcasarin@users.noreply.github.com>
2022-10-15 17:52:45 -0300

    added AppBar

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -4,11 +4,16 @@ import { Box, Text, Button } from 'native-base';
 
 import type { HomeScreenProps } from '_navigators';
 
+import { AppBar } from '_components/misc';
+
 export const HomeScreen = ({ navigation }: HomeScreenProps) => {
   return (
-    <Box flex={1} bg="#fff" alignItems="center" justifyContent="center">
-      <Text>HOME</Text>
-      <Button onPress={() => navigation.navigate('Login')}>Click Me</Button>
-    </Box>
+    <>
+      <AppBar />
+      <Box flex={1} bg="#fff" alignItems="center" justifyContent="center">
+        <Text>HOME</Text>
+        <Button onPress={() => navigation.navigate('Login')}>Click Me</Button>
+      </Box>
+    </>
   );
 };
--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -3,7 +3,13 @@ import React from 'react';
 import type { LoginScreenProps } from '_navigators';
 
 import { FormLogin } from '_components/forms';
+import { AppBar } from '_components/misc';
 
 export const LoginScreen = ({}: LoginScreenProps) => {
-  return <FormLogin />;
+  return (
+    <>
+      <AppBar />
+      <FormLogin />
+    </>
+  );
 };
