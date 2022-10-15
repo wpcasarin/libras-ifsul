4f56892ff8ec3342ae04a7ac68a425b453e5905d
wpcasarin <72427968+wpcasarin@users.noreply.github.com>
2022-10-15 16:47:58 -0300

    icons support

---
+++ src/icons/index.ts
@@ -0,0 +1,5 @@
+import Feather from 'react-native-vector-icons/Feather';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+
+export { Feather, Ionicons, MaterialIcons };
